6f53f1da88c90060979af919e36fd8e079661b4c
Breno Hideki Utimura <94381719+BrenoUtimura@users.noreply.github.com>
2025-05-21 15:08:57 -0300

    Add files via upload

--- funcoes.c
+++ funcoes.c
@@ -70,7 +70,7 @@ void operacao(int op){
 
 void calc_mat(int mat){
 
-    int resp;
+    int resp, resp2, hipot, cateto, cat1, cat2;
 
     resp = 0;
 
@@ -91,74 +91,97 @@ void calc_mat(int mat){
         printf("----------------------------------------------------\n");
         printf("6 - Logaritmo\n");
         printf("----------------------------------------------------\n");
-        printf("7 - Distancia entre dois pontos\n");
+        printf("7 - Radiciacao\n");
         printf("----------------------------------------------------\n");
-        printf("8 - Equacao do Primeiro Grau\n");
+        printf("8 - Distancia entre dois pontos\n");
         printf("----------------------------------------------------\n");
-        printf("9 - Equacao do Segundo Grau\n");
+        printf("9 - Equacao do Primeiro Grau\n");
         printf("----------------------------------------------------\n");
-        printf("10 - Funcao do Primeiro Grau\n");
+        printf("10 - Equacao do Segundo Grau\n");
         printf("----------------------------------------------------\n");
-        printf("11 - Funcao do Segundo Grau\n");
+        printf("11 - Funcao do Primeiro Grau\n");
         printf("----------------------------------------------------\n");
-        printf("12 - Funcao Exponencial\n");
+        printf("12 - Funcao do Segundo Grau\n");
         printf("----------------------------------------------------\n");
-        printf("13 - Funcao Logaritma\n");
+        printf("13 - Funcao Exponencial\n");
+        printf("----------------------------------------------------\n");
+        printf("14 - Funcao Logaritma\n");
         printf("----------------------------------------------------\n");
 
         scanf("%d", &resp);
 
         switch (resp)
         {
-            case 1:
+            case 1: /* Dizima Periódica */
                 /* code */
             break;
         
-            case 2:
+            case 2: /* Teorema de Pitogoras */
+                printf("Vai ter que definir o valor da hipotenusa?\n");
+                printf("1 - Sim   e   2 - Não");
+                scanf("%d", &resp2);
+
+                if(resp2 = 1){
+                    printf("Informe os valores dos catetos para definir a Hipotenusa\n");
+                    printf("Cateto1:  ");
+                    scanf("%d", &cat1);
+
+                    printf("Cateto2:  ");
+                    scanf("%d", &cat2);
+
+                    cateto = pow(cat1, 2) + pow(cat2, 2);
+                    hipot = pow(cateto, 2);
+
+                    printf("%d", hipot);
+                }
+
+            break;
+
+            case 3: /* Areas */
                 /* code */
             break;
 
-            case 3:
+            case 4: /* Potenciação */
                 /* code */
             break;
 
-            case 4:
+            case 5: /* Exponenciação */
                 /* code */
             break;
 
-            case 5:
+            case 6: /* Logaritmo */
                 /* code */
             break;
 
-            case 6:
+            case 7: /* Radiciação */
                 /* code */
             break;
 
-            case 7:
+            case 8: /* Distancia entre dois pontos */
                 /* code */
             break;
 
-            case 8:
+            case 9: /* Equação do primeiro grau */
                 /* code */
             break;
 
-            case 9:
+            case 10: /* Equação do segundo grau */
                 /* code */
             break;
 
-            case 10:
+            case 11: /* Função do primeiro grau */
                 /* code */
             break;
 
-            case 11:
+            case 12: /* Função do segundo grau */
                 /* code */
             break;
 
-            case 12:
+            case 13: /* Função Exponencial */
                 /* code */
             break;
 
-            case 13:
+            case 14: /* Função Logarítma */
                 /* code */
             break;
         }
